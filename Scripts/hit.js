656992a65fbd52b61244ed20de802dd00dc0fae6
Rahul <webdev.rahul@gmail.com>
2025-07-17 15:36:19 +0530

    old website
    old website

---
+++ Scripts/hit.js
@@ -0,0 +1,14 @@
+﻿$(function () {
+
+    $('.bxslider').bxSlider({
+        auto: true,
+        autoControls: true,
+        mode: 'fade',
+        captions: true
+    });
+    
+    $('a').click(function () {
+        //alert("Handler for .click() called.");
+        //return false;
+    });
+});
